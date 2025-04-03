3309a1b9641adfdb1213cff2a106de7a30bcad5f
Ronan Shaughnessy <rshocks297@gmail.com>
2025-04-03 17:24:05 +0100

    feature/lookup-function-implementation

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::{env, net::IpAddr, str::FromStr};
+
+/* help
+cargo run --
+Port_Lookup.exe -h
+Port_Lookup.exe -j 100 192.168.1.1
+Port_Lookup.exe 192.168.1.1
+*/
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        } else if args.len() > 4 {
+            return Err("too many arguments");
+        }
+        let f = args[1].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f) {
+            return Ok(Arguments {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
+        } else {
+            let flag = args[1].clone();
+            if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
+                println!(
+                    "Usage: -j to select how many threads you want
+                \n\r       -h or -help to show this help message"
+                );
+                return Err("help");
+            } else if flag.contains("-h") || flag.contains("-help") {
+                return Err("too many arguments");
+            } else if flag.contains("-j") {
+                let ipaddr = match IpAddr::from_str(&args[3]) {
+                    Ok(s) => s,
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
+                };
+                let threads = match args[2].parse::<u16>() {
+                    Ok(s) => s,
+                    Err(_) => return Err("failed to parse thread number"),
+                };
+                return Ok(Arguments {
+                    threads,
+                    flag,
+                    ipaddr,
+                });
+            } else {
+                return Err("invalid syntax");
+            }
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+}
